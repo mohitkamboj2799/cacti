228340d59f5f96464091436ad1a04761aa6b7a60
mohitkamboj2799 <133959924+mohitkamboj2799@users.noreply.github.com>
2023-10-20 12:18:22 +0530

    Update cacti-setup-wizard-debian-Ubuntu.sh

--- cacti-setup-wizard-debian-Ubuntu.sh
+++ cacti-setup-wizard-debian-Ubuntu.sh
@@ -64,10 +64,7 @@ apt-get  install -y apache2 libapache2-mod-php  rrdtool mariadb-server snmp snmp
 
 
 
-echo "will you be using the spine poller enter 1 for yes 2 for no"
-read answer
-if [ $answer == "1" ]
-then
+
 ##Download packages needed for spine
 apt-get  install -y build-essential dos2unix dh-autoreconf libtool  help2man libssl-dev libmysql++-dev  librrds-perl libsnmp-dev 
 echo "downloading and compling spine"
